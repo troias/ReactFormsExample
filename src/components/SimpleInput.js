2f20994207262d7eb0a73c62ba5e91c9af5e8f39
troy <hunter007nz@gmail.com>
2021-07-15 21:31:46 +1200

    added handler functions

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,30 @@
+import {useRef, useState} from "react"
+
 const SimpleInput = (props) => {
+
+
+  const nameInputRef = useRef()
+  const [enteredName, setEnteredName] = useState("")
+
+  const addInputNameHandler = (event) => {
+    event.preventDefault()
+    setEnteredName(event.target.value)
+    console.log(enteredName)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    // setEnteredName(event.target.value)
+    const enteredValue = nameInputRef.current.value
+    nameInputRef.current.value = ""
+    console.log(enteredValue)
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <label htmlFor='name'> Your Name </label>
+        <input  ref={nameInputRef}  type='text' id='name' onChange={addInputNameHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
